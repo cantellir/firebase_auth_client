b7a2abdfd59a660516c1b551450acf2b8d8a8cd9
Rodrigo <cantelli.r@gmail.com>
2021-08-31 15:28:43 -0300

    add export

--- lib/firebase_auth_client.dart
+++ lib/firebase_auth_client.dart
@@ -1,4 +1,5 @@
 export 'src/auth_exception.dart';
 export 'src/factories/make_firebase_auth_client.dart';
+export 'src/firebase_auth_client.dart';
 export 'src/firebase_auth_strings.dart';
 export 'src/firebase_user.dart';
